da9e5feb8b589663b8e8229767fa87e5d7e177a0
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-08-09 19:58:33 +0530

    style: layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Change font size</h1>
-<div>
-  
-</div>
-`;
+var txtArea = document.querySelector("#txt-area");
+var btnDecrease = document.querySelector("#btn-decrease");
+var btnIncrease = document.querySelector("#btn-increase");
+var txtOutput = document.querySelector("#output");
+
+txtArea.addEventListener("change", () => {
+  var outputText = txtArea.value;
+  txtOutput.innerText = outputText;
+});
