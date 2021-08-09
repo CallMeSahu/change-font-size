9db84651f4d127862937bd761c318fe0708720af
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-08-09 20:54:53 +0530

    feat: functionality

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ var txtArea = document.querySelector("#txt-area");
 var btnDecrease = document.querySelector("#btn-decrease");
 var btnIncrease = document.querySelector("#btn-increase");
 var txtOutput = document.querySelector("#output");
+var sizeOutput = document.querySelector("#size-output");
+var initialSize = 20;
 
 txtArea.addEventListener("change", () => {
   var outputText = txtArea.value;
   txtOutput.innerText = outputText;
+});
+
+btnIncrease.addEventListener("click", () => {
+  initialSize += 2;
+  var finalSize = String(initialSize) + "px";
+  sizeOutput.innerText = finalSize;
+  txtOutput.style.fontSize = finalSize;
+});
+
+btnDecrease.addEventListener("click", () => {
+  initialSize -= 2;
+  var finalSize = String(initialSize) + "px";
+  sizeOutput.innerText = finalSize;
+  txtOutput.style.fontSize = finalSize;
 });
